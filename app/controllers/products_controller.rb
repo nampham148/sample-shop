6ed30fe20142c7eb8daf4cc305582c7eaab02ba5
nampham148 <phamquangnam148@gmail.com>
2018-06-18 08:49:18 +0700

    fix product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,15 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
-  	category_id = params[:category_id]
-  	if category_id
-  		@products = Category.find(category_id).products.paginate(page: params[:page])
-  	else
-  		@products = Product.all.paginate(page: params[:page])
-  	end
+    category_id = params[:category_id]
+    if category_id
+      @products = Category.find(category_id).products.paginate(page: params[:page])
+    else
+      @products = Product.all.paginate(page: params[:page])
+    end
   end
 
   def show
-  	@product = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
   end
 end
